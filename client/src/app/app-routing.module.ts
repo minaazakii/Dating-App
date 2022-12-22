423a42d840c6bc973420c197fb2793a5a2406e7e
minaazakii <minaeragon@gmail.com>
2022-12-22 17:33:20 +0200

    end of section 10

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { HomeComponent } from './_services/home/home.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { NgModule } from '@angular/core';
@@ -9,6 +10,7 @@ import { AuthGuard } from './_guards/auth.guard';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
 const routes: Routes =
 [
@@ -18,8 +20,9 @@ const routes: Routes =
     runGuardsAndResolvers:'always',
     canActivate:[AuthGuard],
     children:[
-      {path:'members',component:MemberListComponent,canActivate:[AuthGuard]},
+      {path:'members',component:MemberListComponent},
       {path:'members/:username',component:MemberDetailComponent},
+      {path:'member/edit',component:MemberEditComponent,canDeactivate:[PreventUnsavedChangesGuard]},
       {path:'lists',component:ListsComponent},
       {path:'messages',component:MessagesComponent},
     ]
